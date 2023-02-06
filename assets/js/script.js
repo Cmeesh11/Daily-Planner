da82768a76c8198f4b26425c501a67f90bb8dd1b
Cmeesh11 <cartermeesh11@gmail.com>
2023-02-05 21:24:15 -0500

    Can now store values in localstoage, re-render them on the page, and shows date at top of page

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,14 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+// use the id in the containing time-block as a key to save the user input in
+// local storage. HINT: What does `this` reference in the click listener
+// function? How can DOM traversal be used to get the "hour-x" id of the
+// time-block containing the button that was clicked? How might the id be
+// useful when saving the description in local storage?
 
 $(function () {
   var container = $(".container-lg");
-
-  
 
   // Set a starting time for typical work day
   var startTime = dayjs().hour(9).minute(0).second(0);
@@ -23,18 +21,28 @@ $(function () {
     var textarea = $("<textarea>");
     var iTag = $("<i>");
 
+    
     // Add rest of elements
     box.addClass("col-2 col-md-1 hour text-center py-3");
     box.attr("id", i + 9);
     box.text(startTime.add(i, "hour").format("h A"));
 
-    // textarea.text(localStorage.getItem("notes")[i]);
     textarea.addClass("col-8 col-md-10 description");
+    textarea.attr("data-hour", startTime.add(i, "hour").format("h"));
     textarea.attr("rows", "3");
 
     button.addClass("btn saveBtn col-2 col-md-1");
     button.attr("data-hour", startTime.add(i, "hour").format("h"));
     button.attr("aria-label", "save");
+    
+    $(button).each(function () {
+      var $this = $(this);
+      $this.on("click", function () {
+        var buttonClicked = $this.data("hour");
+        // Save in local storage for the button clicked
+        localStorage.setItem(buttonClicked, $this.siblings("textarea").val());
+      });
+    });
 
     iTag.addClass("fas fa-save");
     iTag.attr("aria-hidden", "true");
@@ -54,15 +62,11 @@ $(function () {
     else if (startTime.add(i, "hour").isAfter(dayjs(), "hour")) {
       row.addClass("row time-block future");
     }
+    // Get values from local storage and put them in the textareas
+    textarea.val(localStorage.getItem(button.data("hour")));
   }
-  // Only triggers listener if one of the saveBtns are clicked
-  container.on("click", container.children().children(".saveBtn"), function () {
   
-  // Here is the line that I'm having trouble with. Returns "9" no matter which save button I click on
-  console.log($(this).children().children(box).attr("id"));
-  // Define a dom variable for the box using the id and a button variable using the data-hour value
-
-  });
-
-  // TODO: Add code to display the current date in the header of the page.
 });
+
+// TODO: Add code to display the current date in the header of the page.
+$("#currentDay").text(dayjs().format("[Today is]  MMM DD, YYYY"));
